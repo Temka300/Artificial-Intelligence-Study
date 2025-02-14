a99213493271805b1ae5a6a4a13a2b2795286e91
Temka300 <temkaserge@gmail.com>
2025-02-14 19:05:06 +0800

    Add initial implementation of Linear Regression, PyTorch, and TensorFlow examples

---
+++ pytorchex.py
@@ -0,0 +1,31 @@
+#@save
+import collections
+import hashlib
+import inspect
+import math
+import os
+import random
+import re
+import shutil
+import sys
+import tarfile
+import time
+import zipfile
+from collections import defaultdict
+import pandas as pd
+import requests
+from IPython import display
+from matplotlib import pyplot as plt
+from matplotlib_inline import backend_inline
+
+d2l = sys.modules[__name__]
+
+#@save
+import numpy as np
+import torch
+import torchvision
+from PIL import Image
+from scipy.spatial import distance_matrix
+from torch import nn
+from torch.nn import functional as F
+from torchvision import transforms
